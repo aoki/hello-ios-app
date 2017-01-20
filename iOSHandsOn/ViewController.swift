3697a02fa76ab71e316b0971e084cbac74cdba44
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2017-01-20 19:11:20 +0900

    Initial Commit

---
+++ iOSHandsOn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSHandsOn
+//
+//  Created by Yoshiki Aoki on 2017/1/20.
+//  Copyright © 2017 Yoshiki Aoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
